368624e93d3d9f4e7f81920001f2194c89c2c502
Scott <scott.michel@hotmail.fr>
2019-10-08 11:21:46 +0200

    correction

--- backend/routes/recetteRoutes.js
+++ backend/routes/recetteRoutes.js
@@ -4,12 +4,24 @@ const express = require ('express');
 //Création du routeur
 const router = express.Router();
 
+//Import du module de connexion à la base de données
+const connection = require('../connexionMysql')
+
 
 
 //Liste des recettes
 router.get("/", (req,res) => {
-    connection.query('SELECT * FROM recette', (err, res))
-    res.json(data)
+    connection.query('SELECT * FROM recette', (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(500).send("Impossible d'exécuter la requête");
+        } else { 
+            res.json(data) 
+        }
+            
+        
+    })
+    
 });
 
 
