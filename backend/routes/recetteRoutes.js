af8da4be75528ba3005a18ab89228ce9f77332fb
Scott <scott.michel@hotmail.fr>
2019-10-08 11:06:19 +0200

    router.get SELECT * FROM

--- backend/routes/recetteRoutes.js
+++ backend/routes/recetteRoutes.js
@@ -4,4 +4,14 @@ const express = require ('express');
 //Création du routeur
 const router = express.Router();
 
+
+
+//Liste des recettes
+router.get("/", (req,res) => {
+    connection.query('SELECT * FROM recette', (err, res))
+    res.json(data)
+});
+
+
+//Exportation du routeur
 module.exports = router;
