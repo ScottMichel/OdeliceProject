b0f1409c5be00e1a93d5e766364e572005c79476
Sebastien Maloron <contact@smaloron.eu>
2019-10-09 16:23:27 +0200

    Corrction des fixtures pour les recettes

--- backend/routes/recetteRoutes.js
+++ backend/routes/recetteRoutes.js
@@ -1,5 +1,5 @@
 // Importation d'express
-const express = require ('express');
+const express = require('express');
 
 //Création du routeur
 const router = express.Router();
@@ -8,71 +8,71 @@ const router = express.Router();
 const connection = require('../connexionMysql')
 
 router.get('/test', (req, res) => {
-    connection.query('SELECT * FROM commentaire', (err, data) =>{
-        if(err){
+    connection.query('SELECT * FROM commentaire', (err, data) => {
+        if (err) {
             console.log(err)
         } else {
             console.log(data)
             res.json(data);
         }
-       
+
     })
 });
 
 //Liste de toutes les recettes
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
     connection.query('SELECT * FROM odelice.recette', (err, data) => {
-        if(err){
-            console.log(err);
-            res.status(500).send("Impossible d'exécuter la requête");
-        } else{
-            console.log('test');
-            res.json('hello');
-        }
-    })
-    
-});
-
-//Recherche une recette dans la barre url
-router.get('/search', (req,res) => {
-    console.log('param : ' + req.query.titre)
-    connection.query("SELECT * FROM recette WHERE titre LIKE ?", ['%' + req.query.titre + '%'],
-    (err, data) => {
-        console.log('callback');
-        if(err){
+        if (err) {
             console.log(err);
             res.status(500).send("Impossible d'exécuter la requête");
         } else {
-            console.log(data);
-            res.json(data)
+            console.log('test');
+            res.json(data);
         }
-    });
+    })
+
+});
+
+//Recherche une recette dans la barre url
+router.get('/search', (req, res) => {
+    console.log('param : ' + req.query.titre)
+    connection.query("SELECT * FROM recette WHERE titre LIKE ?", ['%' + req.query.titre + '%'],
+        (err, data) => {
+            console.log('callback');
+            if (err) {
+                console.log(err);
+                res.status(500).send("Impossible d'exécuter la requête");
+            } else {
+                console.log(data);
+                res.json(data)
+            }
+        });
 });
 
 //Liste d'une seule recette
 router.get('/:id', (req, res, next) => {
     connection.query('SELECT * FROM recette WHERE idRecette=?', [req.params.id],
-    (err,data) => {
-        if(err){
-            console.log(err);
-            res.status(500).send("Impossible d'exécuter la requête");
-        } else {
-            res.json(data)
-        };
-    })
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Impossible d'exécuter la requête");
+            } else {
+                res.json(data)
+            };
+        })
 })
 
 //Liste des commentaires par recette
 router.get('/commentaire/:id', (req, res) => {
     connection.query('SELECT * FROM commentaire WHERE idRecette=?', [req.params.id],
-    (err,data) => {
-        if(err){
-            console.log(err);
-            res.status(500).send("Impossible d'exécuter la requête");
-        } else {
-            res.json(data)
-        };
-    });
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Impossible d'exécuter la requête");
+            } else {
+                res.json(data)
+            };
+        });
 });
 
 
